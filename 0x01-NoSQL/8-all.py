260fb35e9cb7d76fda87b22231109607f2e13d4e
MoedCode <mohamed71291@gmail.com>
2024-05-01 03:17:55 +0300

    0x01-NoSQL  Tasks from 8  to  THE END

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+function that list all document
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    list all collections
+    """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
