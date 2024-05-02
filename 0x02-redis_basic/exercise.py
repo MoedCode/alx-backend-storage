2ba6a4640aa59ca347af82133b079ac504e8af8d
MoedCode <mohamed71291@gmail.com>
2024-05-02 04:46:22 +0300

    0x02-redis_basic T0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """cach module"""
 
 import redis
@@ -47,7 +47,7 @@ def replay(method: Callable) -> None:
 
 
 class Cache:
-    """Cach Class"""
+    """ Initializes a Cache object with a Redis client instance. shift + 2 arrows"""
 
     def __init__(self):
         """Constructor"""
@@ -62,23 +62,13 @@ class Cache:
         self._redis.set(dataKey, data)
         return dataKey
 
-    def get(
-            self,
-            key: str,
-            fn: Callable = None,
-    ) -> Union[str, bytes, int, float]:
-        """Get data from redis"""
-        data = self._redis.get(key)
-        if fn:
-            return fn(data)
-        return data
 
-    def get_str(self, dataKey: str) -> str:
-        """Get data from redis as string"""
-        data = self._redis.get(dataKey)
-        return data.decode("utf-8")
+if __name__ == "__main__":
+    cache = Cache()
 
-    def get_int(self, dataKey: str) -> int:
-        """Get data from redis as integer"""
-        data = self._redis.get(dataKey)
-        return int(data)
+    data = b"hello"
+    key = cache.store(data)
+    print(key)
+
+    local_redis = redis.Redis()
+    print(local_redis.get(key))
