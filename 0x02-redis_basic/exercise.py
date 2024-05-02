2d28c00b1db1036ac26151e51ac5519c46da343c
MoedCode <mohamed71291@gmail.com>
2024-05-02 03:59:52 +0300

    0x02-redis_basic T0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,9 @@ from typing import Union
     a private variable named _redis (using redis.Redis())
     and flush the instance using flushdb.
 """
-class Cache :
+
+
+class Cache:
     """
     Initializes a Cache object with a Redis client instance.
     shift + 2 arrows
@@ -16,10 +18,13 @@ class Cache :
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-    def store(self, data:Union[str, bytes, int, float]) -> str:
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         rnd_key = str(uuid.uuid4())
         self._redis.set(rnd_key, data)
         return rnd_key
+
+
 if __name__ == "__main__":
     cache = Cache()
 
