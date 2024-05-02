504552cef359fb6291cd9f195ba9618979951947
MoedCode <mohamed71291@gmail.com>
2024-05-02 04:34:19 +0300

    0x02-redis_basic T0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """cach module"""
+
 import redis
 import uuid
 from typing import Union
@@ -9,10 +10,12 @@ class Cache:
     """ Initializes a Cache object with a Redis client instance. shift + 2 arrows"""
 
     def __init__(self):
+        """ constructor """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in redis"""
         rnd_key = str(uuid.uuid4())
         self._redis.set(rnd_key, data)
         return rnd_key
