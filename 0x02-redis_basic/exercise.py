df1aecc97d83e9ba9beb3f0a274ad7719b460ebd
MoedCode <mohamed71291@gmail.com>
2024-05-02 04:28:02 +0300

    0x02-redis_basic T0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,41 +1,9 @@
 #!/usr/bin/python3
 import redis
 import uuid
-from typing import Any, Callable, Union
-from functools import wraps
+from typing import Union
 
-"""
-    Cache class. In the __init__ method, store an instance of the Redis client as
-    a private variable named _redis (using redis.Redis())
-    and flush the instance using flushdb.
-"""
-
-
-def count_calls(method: Callable) -> Callable:
-    """Decorator that counts how many times a function is called"""
-    key = method.__qualname__
-
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """Wrapper function"""
-        self._redis.incr(key)
-        return method(self, *args, **kwargs)
-    return wrapper
-
-
-def call_history(method: Callable) -> Callable:
-    """Decorator that stores the history of inputs
-    and outputs for a function"""
-    key = method.__qualname__
-
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """Wrapper function"""
-        self._redis.rpush(f"{key}:inputs", str(args))
-        result = method(self, *args, **kwargs)
-        self._redis.rpush(f"{key}:outputs", str(result))
-        return result
-    return wrapper
+""" Cash module"""
 
 
 class Cache:
@@ -48,8 +16,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
-    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         rnd_key = str(uuid.uuid4())
         self._redis.set(rnd_key, data)
